8e3764416721d9c8a8b907c31328c322f8153b3f
Asa Paparo <asapaparo@gmail.com>
2023-08-04 09:29:22 -0400

    spacing

--- talkingclock.py
+++ talkingclock.py
@@ -74,14 +74,14 @@ class Solution:
             minute_word = ""
 
             if minute // 10 in prefixes.keys():
-                minute_word = f"{prefixes[minute // 10] if minute // 10 in prefixes.keys() else words[minute // 10]} {words[minute % 10]}"
+                minute_word = f" {prefixes[minute // 10] if minute // 10 in prefixes.keys() else words[minute // 10]} {words[minute % 10]}"
             else:
-                minute_word = words[minute]
+                minute_word = f" {words[minute]}"
 
             if minute_word == "oh":
                 minute_word = ""
 
-            return f"It's {words[hour]} {minute_word} {am}"
+            return f"It's {words[hour]}{minute_word} {am}"
 
 def main():
      str1=input()
